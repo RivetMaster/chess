1f4fe903a99e3658f3d16fa892d831c7e32c24d2
RivetMaster <gish.loves.chocolate@gmail.com>
2026-05-07 17:57:04 -0400

    improve readability of ChessPieceMoves

--- shared/src/main/java/chess/ChessPieceMoves.java
+++ shared/src/main/java/chess/ChessPieceMoves.java
@@ -48,42 +48,39 @@ public class ChessPieceMoves {
         possibleMoves = new ArrayList<>();
         int row = position.getRow();
         int col = position.getColumn();
+        int sq_depth = 8;
+        int black_pwn_row = 7;
+        int white_pwn_row = 2;
         ChessPosition temp;
         ChessGame.TeamColor opponent = WHITE;
         if(pieceColor == WHITE) {
             opponent = BLACK;
         }
-        //Rook moves
         if(type==PAWN){
-            //if white pawn
             if(pieceColor == WHITE){
-                //move 1 forward with no promotion
-                if(row < 7) {
+                if(row < black_pwn_row) { //move no promotion
                     temp = new ChessPosition(row + 1, col);
                     if (board.getPiece(temp) == null) {
                         possibleMoves.add(new ChessMove(position, temp, null));
                     }
-                } else if (row == 7){
-                    //move 1 forward with all options for promotion
-                    temp = new ChessPosition(row+1, col);
+                } else if (row == black_pwn_row){ // move with promotion options
+                    temp = new ChessPosition(row + 1, col);
                     if (board.getPiece(temp) == null) {
                         for(ChessPiece.PieceType t : promotionTypes){
                             possibleMoves.add(new ChessMove(position, temp, t));
                         }
                     }
                 }
-                if(row == 2) { //only time pawn still at row 2 is when can move forward 2 or 1
+                if(row == white_pwn_row) { //move 2 forward
                     temp = new ChessPosition(row + 2, col);
                     if (board.getPiece(temp) == null && board.getPiece(new ChessPosition(row + 1, col)) == null) {
                         possibleMoves.add(new ChessMove(position, temp, null));
                     }
                 }
-                // ADD MOVE DIAGONAL TO CAPTURE
-                if(col > 1){
-                    //check left diagonal
+                if(col > 1){ // diagonal capture left
                     temp = new ChessPosition(row + 1, col - 1);
-                    if(board.getPiece(temp) != null && (board.getPiece(temp)).getTeamColor() == BLACK){
-                        if(row == 7){
+                    if(board.getPiece(temp) != null && (board.getPiece(temp)).getTeamColor() == opponent){
+                        if(row == black_pwn_row){ //diagonal promotion
                             for(ChessPiece.PieceType t : promotionTypes){
                                 possibleMoves.add(new ChessMove(position, temp, t));
                             }
@@ -92,11 +89,10 @@ public class ChessPieceMoves {
                         }
                     }
                 }
-                if(col < 8){
-                    //check right diagonal
+                if(col < sq_depth){ //diagonal capture right
                     temp = new ChessPosition(row + 1, col + 1);
-                    if(board.getPiece(temp) != null && (board.getPiece(temp)).getTeamColor() == BLACK){
-                        if(row == 7){
+                    if(board.getPiece(temp) != null && (board.getPiece(temp)).getTeamColor() == opponent){
+                        if(row == black_pwn_row){ //diagonal promotion
                             for(ChessPiece.PieceType t : promotionTypes){
                                 possibleMoves.add(new ChessMove(position, temp, t));
                             }
@@ -105,16 +101,13 @@ public class ChessPieceMoves {
                         }
                     }
                 }
-
-            //if black pawn
             } else if(pieceColor == BLACK){
-                if(row > 2){
+                if(row > white_pwn_row){ //move forward
                     temp = new ChessPosition(row-1, col);
                     if (board.getPiece(temp) == null) {
                         possibleMoves.add(new ChessMove(position, temp, null));
                     }
-                } else if (row == 2){
-                    //move 1 forward with all options for promotion
+                } else if (row == white_pwn_row){ // move forward promotion
                     temp = new ChessPosition(row-1, col);
                     if (board.getPiece(temp) == null) {
                         for (ChessPiece.PieceType t : promotionTypes) {
@@ -122,18 +115,16 @@ public class ChessPieceMoves {
                         }
                     }
                 }
-                if(row == 7) { //only time pawn still at row 7 is when can move forward 2 or 1
+                if(row == black_pwn_row) { // move forward 2
                     temp = new ChessPosition(row - 2, col);
                     if (board.getPiece(temp) == null && board.getPiece(new ChessPosition(row-1, col)) == null) {
                         possibleMoves.add(new ChessMove(position, temp, null));
                     }
                 }
-                //ADD MOVE DIAGONAL TO CAPTURE
-                if(col > 1){
-                    //check left diagonal
+                if(col > 1){ // capture diagonal left
                     temp = new ChessPosition(row - 1, col - 1);
-                    if(board.getPiece(temp) != null && (board.getPiece(temp)).getTeamColor() == WHITE){
-                        if(row == 2){
+                    if(board.getPiece(temp) != null && (board.getPiece(temp)).getTeamColor() == opponent){
+                        if(row == white_pwn_row){
                             for(ChessPiece.PieceType t : promotionTypes){
                                 possibleMoves.add(new ChessMove(position, temp, t));
                             }
@@ -142,11 +133,10 @@ public class ChessPieceMoves {
                         }
                     }
                 }
-                if(col < 8){
-                    //check right diagonal
+                if(col < sq_depth){ // capture diagonal right
                     temp = new ChessPosition(row - 1, col + 1);
-                    if(board.getPiece(temp) != null && (board.getPiece(temp)).getTeamColor() == WHITE){
-                        if(row == 2){
+                    if(board.getPiece(temp) != null && (board.getPiece(temp)).getTeamColor() == opponent){
+                        if(row == white_pwn_row){
                             for(ChessPiece.PieceType t : promotionTypes){
                                 possibleMoves.add(new ChessMove(position, temp, t));
                             }
@@ -158,49 +148,45 @@ public class ChessPieceMoves {
             }
         }
         if(type == ROOK || type == QUEEN){
-            //search up vertically, adding spots to valid moves up to and including one spot that is occupied, but not past
-            for(int r = row+1; r<9; r++){
+            for(int r = row + 1; r <= sq_depth; r++){ //check every square vertically up from position
                 temp = new ChessPosition(r, col);
-                if(board.getPiece(temp) == null){
+                if(board.getPiece(temp) == null){ //add if square empty
                     possibleMoves.add(new ChessMove(position, temp, null));
                 } else {
-                    if ((board.getPiece(temp)).getTeamColor() == opponent){
+                    if ((board.getPiece(temp)).getTeamColor() == opponent){ //add iff opponent but don't add any past
                         possibleMoves.add(new ChessMove(position, temp, null));
                     }
                     break;
                 }
             }
-            //search down vertically, adding spots to valid moves up to and including one spot that is occupied, but not past
-            for(int r = row-1; r>0; r--){
+            for(int r = row - 1; r > 0; r--){ //check every square vertically down from position
                 temp = new ChessPosition(r, col);
-                if(board.getPiece(temp) == null){
+                if(board.getPiece(temp) == null){ //add if square empty
                     possibleMoves.add(new ChessMove(position, temp, null));
                 } else{
-                    if ((board.getPiece(temp)).getTeamColor() == opponent){
+                    if ((board.getPiece(temp)).getTeamColor() == opponent){ //add iff opponent but don't add any past
                         possibleMoves.add(new ChessMove(position, temp, null));
                     }
                     break;
                 }
             }
-            //search right, adding spots to valid moves up to and including one spot that is occupied, but not past
-            for(int c = col+1; c<9; c++){
+            for(int c = col + 1; c <= sq_depth; c++){ //check every square horizontally right from position
                 temp = new ChessPosition(row, c);
-                if(board.getPiece(temp) == null){
+                if(board.getPiece(temp) == null){ //add if square empty
                     possibleMoves.add(new ChessMove(position, temp, null));
                 } else {
-                    if ((board.getPiece(temp)).getTeamColor() == opponent){
+                    if ((board.getPiece(temp)).getTeamColor() == opponent){ //add iff opponent but don't add any past
                         possibleMoves.add(new ChessMove(position, temp, null));
                     }
                     break;
                 }
             }
-            //search left, adding spots to valid moves up to and including one spot that is occupied, but not past
-            for(int c = col-1; c>0; c--){
+            for(int c = col - 1; c > 0; c--){ //check every square horizontally left from position
                 temp = new ChessPosition(row, c);
-                if(board.getPiece(temp) == null){
+                if(board.getPiece(temp) == null){ //add if square empty
                     possibleMoves.add(new ChessMove(position, temp, null));
                 } else {
-                    if ((board.getPiece(temp)).getTeamColor() == opponent){
+                    if ((board.getPiece(temp)).getTeamColor() == opponent){ //add iff opponent but don't add any past
                         possibleMoves.add(new ChessMove(position, temp, null));
                     }
                     break;
@@ -208,10 +194,9 @@ public class ChessPieceMoves {
             }
         }
         if(type == KNIGHT){
-            //check forward jumps
-            if (row < 7){
+            if (row < black_pwn_row){ // check forward jumps
                 if(col < 7) { //forward 2, right 1
-                    temp = new ChessPosition(row + 2, col +1);
+                    temp = new ChessPosition(row + 2, col + 1);
                     if(board.getPiece(temp) == null){
                         possibleMoves.add(new ChessMove(position, temp, null));
                     } else if (board.getPiece(temp).getTeamColor() == opponent){
@@ -227,8 +212,7 @@ public class ChessPieceMoves {
                     }
                 }
             }
-            // check backwards jumps
-            if(row > 2){
+            if(row > 2){ // check backwards jumps
                 if(col < 7) { //backwards 2, right 1
                     temp = new ChessPosition(row-2, col+1);
                     if(board.getPiece(temp) == null){
@@ -246,8 +230,7 @@ public class ChessPieceMoves {
                     }
                 }
             }
-            //check left jumps
-            if(col > 2){
+            if(col > 2){ //check left jumps
                 if(row < 8){ //jump left 2, forward 1
                     temp = new ChessPosition(row + 1, col - 2);
                     if(board.getPiece(temp) == null){
@@ -265,8 +248,7 @@ public class ChessPieceMoves {
                     }
                 }
             }
-            //check right jumps
-            if(col < 7){
+            if(col < 7){ // check right jumps
                 if(row < 8){ //forward 1, right 2
                     temp = new ChessPosition(row + 1, col + 2);
                     if(board.getPiece(temp) == null){
@@ -286,9 +268,8 @@ public class ChessPieceMoves {
             }
         }
         if(type == BISHOP || type == QUEEN){
-            //diagonal to left and up
-            for(int l = 1; l < col; l++){
-                if(row + l <= 8){
+            for(int l = 1; l < col; l++){ // diagonal up left
+                if(row + l <= sq_depth){
                     temp = new ChessPosition(row + l, col - l);
                     if(board.getPiece(temp) == null){
                         possibleMoves.add(new ChessMove(position, temp, null));
@@ -300,8 +281,7 @@ public class ChessPieceMoves {
                     }
                 }
             }
-            //diagonal to left and down
-            for(int l = 1; l < col; l++){
+            for(int l = 1; l < col; l++){ // diagonal down left
                 if(row - l > 0){
                     temp = new ChessPosition(row - l, col - l);
                     if(board.getPiece(temp) == null){
@@ -315,8 +295,8 @@ public class ChessPieceMoves {
                 }
             }
             //diagonal to right and up
-            for(int l = 1; l <= 8-col; l++){
-                if(row + l < 9){
+            for(int l = 1; l <= sq_depth - col; l++){ // diagonal up right
+                if(row + l <= sq_depth){
                     temp = new ChessPosition(row + l, col + l);
                     if(board.getPiece(temp) == null){
                         possibleMoves.add(new ChessMove(position, temp, null));
@@ -328,8 +308,7 @@ public class ChessPieceMoves {
                     }
                 }
             }
-            //diagonal to right and down
-            for(int l = 1; l <= 8-col; l++){
+            for(int l = 1; l <= 8-col; l++){ // diagonal down right
                 if(row - l > 0){
                     temp = new ChessPosition(row - l, col + l);
                     if(board.getPiece(temp) == null){
@@ -344,7 +323,7 @@ public class ChessPieceMoves {
             }
         }
         if(type == KING){
-            if(row < 8){
+            if(row < sq_depth){ // move forward
                 temp = new ChessPosition(row + 1, col);
                 if(board.getPiece(temp) == null){
                     possibleMoves.add(new ChessMove(position, temp, null));
@@ -353,7 +332,7 @@ public class ChessPieceMoves {
                         possibleMoves.add(new ChessMove(position, temp, null));
                     }
                 }
-                if(col < 8){
+                if(col < 8){ // move diagonal up right
                     temp = new ChessPosition(row + 1, col + 1);
                     if(board.getPiece(temp) == null){
                         possibleMoves.add(new ChessMove(position, temp, null));
@@ -363,7 +342,7 @@ public class ChessPieceMoves {
                         }
                     }
                 }
-                if(col > 1){
+                if(col > 1){ // move diagonal up left
                     temp = new ChessPosition(row + 1, col - 1);
                     if(board.getPiece(temp) == null){
                         possibleMoves.add(new ChessMove(position, temp, null));
@@ -374,7 +353,7 @@ public class ChessPieceMoves {
                     }
                 }
             }
-            if(row > 1){
+            if(row > 1){ // move down
                 temp = new ChessPosition(row - 1, col);
                 if(board.getPiece(temp) == null){
                     possibleMoves.add(new ChessMove(position, temp, null));
@@ -383,7 +362,7 @@ public class ChessPieceMoves {
                         possibleMoves.add(new ChessMove(position, temp, null));
                     }
                 }
-                if(col < 8){
+                if(col < 8){ // move diagonal down right
                     temp = new ChessPosition(row - 1, col + 1);
                     if(board.getPiece(temp) == null){
                         possibleMoves.add(new ChessMove(position, temp, null));
@@ -393,7 +372,7 @@ public class ChessPieceMoves {
                         }
                     }
                 }
-                if(col > 1){
+                if(col > 1){ // move diagonal down left
                     temp = new ChessPosition(row - 1, col - 1);
                     if(board.getPiece(temp) == null){
                         possibleMoves.add(new ChessMove(position, temp, null));
@@ -404,7 +383,7 @@ public class ChessPieceMoves {
                     }
                 }
             }
-            if(col < 8){
+            if(col < 8){ // move straight right
                 temp = new ChessPosition(row, col + 1);
                 if(board.getPiece(temp) == null){
                     possibleMoves.add(new ChessMove(position, temp, null));
@@ -414,7 +393,7 @@ public class ChessPieceMoves {
                     }
                 }
             }
-            if(col > 1){
+            if(col > 1){ // move straight left
                 temp = new ChessPosition(row, col - 1);
                 if(board.getPiece(temp) == null){
                     possibleMoves.add(new ChessMove(position, temp, null));
@@ -427,7 +406,5 @@ public class ChessPieceMoves {
         }
         return possibleMoves;
     }
-
-
 }
  //make helper function that checks if null or enemy there and adds chess position
