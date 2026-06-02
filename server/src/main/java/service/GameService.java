d4ec936abc178046b85b4e4cb80bc7221fa38f86
RivetMaster <gish.loves.chocolate@gmail.com>
2026-06-02 19:25:24 -0400

    Implement createGame method

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,4 +1,26 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import model.GameData;
+
 public class GameService {
+
+    GameDAO gameDAO;
+    AuthDAO authDAO;
+    AuthService authServ;
+
+    public GameService(GameDAO gameDAO, AuthDAO authDAO){
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+        authServ = new AuthService(authDAO);
+    }
+
+    public void createGame(String gameName, String authToken) throws DataAccessException, InvalidAuthTokenException {
+        authServ.verifyAuth(authToken);
+        GameData game = new GameData(gameDAO.newGameID(), null, null, gameName, new ChessGame());
+        gameDAO.createGame(game);
+    }
 }
