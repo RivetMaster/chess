557b880a6599983754f2b5484bce8767dd55b5fc
RivetMaster <gish.loves.chocolate@gmail.com>
2026-06-02 16:55:40 -0400

    Create GameMemoryDao and implement its methods. Add getGameIndex method to GameMemoryDao, add setUser and setGame methods to GameData record

---
+++ server/src/main/java/dataaccess/GameMemoryDAO.java
@@ -0,0 +1,58 @@
+package dataaccess;
+
+import chess.ChessGame;
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameMemoryDAO implements GameDAO{
+    ArrayList<GameData> games;
+
+    public GameMemoryDAO(){
+        games = new ArrayList<>();
+    }
+
+    //Create a new game.
+    public void createGame(GameData game) throws DataAccessException{
+        games.add(game);
+    }
+
+    //Retrieve a specified game with the given game ID.
+    public GameData getGame(int gameID) throws DataAccessException{
+        int index = getGameIndex(gameID);
+        return games.get(index);
+    }
+
+    //Retrieve all games.
+    public List<GameData> listGames() throws DataAccessException{
+        return games;
+    }
+
+    //Updates a chess game. It should replace the chess game string corresponding to a given gameID.
+    // This is used when players join a game or when a move is made.
+    public void updateGame(int gameID, ChessGame game) throws DataAccessException{
+        int index = getGameIndex(gameID);
+        games.set(index, games.get(index).setGame(game));
+    }
+
+    //Add player to a game (gameID) as the team (color)
+    public void addPlayer(int gameID, ChessGame.TeamColor color, String username) throws DataAccessException{
+        int index = getGameIndex(gameID);
+        games.set(index, games.get(index).setUser(username, color));
+    }
+
+    //clear all games
+    public void clearGames(){
+        games.clear();
+    }
+
+    private int getGameIndex(int gameID) throws DataAccessException{
+        for(int i = 0; i < games.size(); i++){
+            if(games.get(i).gameID() == gameID){
+                return i;
+            }
+        }
+        throw new DataAccessException("Game Does Not Exist");
+    }
+}
--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,7 +1,20 @@
 package model;
 
 import chess.ChessGame;
+import chess.ChessGame.TeamColor;
+
+import static chess.ChessGame.TeamColor.*;
 
 public record GameData(int gameID, String whiteUsername, String blackUsername,
                        String gameName, ChessGame game) {
+    public GameData setUser(String username, TeamColor color){
+        if(color == WHITE){
+            return new GameData(gameID, username, blackUsername, gameName, game);
+        }
+        return new GameData(gameID, whiteUsername, username, gameName, game);
+    }
+
+    public GameData setGame(ChessGame newGame){
+        return new GameData(gameID, whiteUsername, blackUsername, gameName, newGame);
+    }
 }
