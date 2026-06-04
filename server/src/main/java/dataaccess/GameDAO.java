af3eee50b5fb36ef29d79e2d49c3c7a4c5cb2825
RivetMaster <gish.loves.chocolate@gmail.com>
2026-06-03 21:25:52 -0400

    Changed updateGame to take in GameData instead of ChessGame for intended functionality.

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -14,7 +14,7 @@ public interface GameDAO {
     ArrayList<GameData> listGames() throws DataAccessException;
     //Updates a chess game. It should replace the chess game string corresponding to a given gameID.
         // This is used when players join a game or when a move is made.
-    void updateGame(int gameID, ChessGame game) throws DataAccessException;
+    void updateGame(int gameID, GameData game) throws DataAccessException;
     //Add player to a game (gameID) as the team (color)
     void addPlayer(int gameID, ChessGame.TeamColor color, String username) throws DataAccessException;
     //clear all games
