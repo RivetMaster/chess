dbe8c5402e94fd3c70b67f0f25a42385682dc040
RivetMaster <gish.loves.chocolate@gmail.com>
2026-06-05 12:13:51 -0400

    remove unused InvalidAuthTokenException constructor.

--- server/src/main/java/service/exceptions/InvalidAuthTokenException.java
+++ server/src/main/java/service/exceptions/InvalidAuthTokenException.java
@@ -4,7 +4,4 @@ public class InvalidAuthTokenException extends Exception {
     public InvalidAuthTokenException(String message) {
         super(message);
     }
-    public InvalidAuthTokenException(String message, Throwable ex) {
-        super(message, ex);
-    }
 }
