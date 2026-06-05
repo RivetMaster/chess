1332323006010789fc1babfa2079efb6e2e38d96
RivetMaster <gish.loves.chocolate@gmail.com>
2026-06-05 12:18:01 -0400

    Update test to expect InvalidLogInException

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -7,6 +7,7 @@ import model.AuthData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import service.exceptions.InvalidAuthTokenException;
+import service.exceptions.InvalidLogInException;
 import service.exceptions.UnavailableException;
 import service.resultsandrequests.logInRequest;
 import service.resultsandrequests.logOutRequest;
@@ -35,13 +36,13 @@ public class UserServiceTest {
     //log in invalid negative test
     @Test
     void logInNotRegistered () {
-        assertThrows(UnavailableException.class, () ->
+        assertThrows(InvalidLogInException.class, () ->
                 service.logIn(new logInRequest("Betty", "1234")));
     }
 
     //register same username twice fail, negative test
     @Test
-    void registerSameNameTwice() throws UnavailableException, DataAccessException {
+    void registerSameNameTwice() throws UnavailableException, DataAccessException, InvalidLogInException {
         service.register(new registerUserRequest("Ann", "5678", "hit@gmail.com"));
         assertThrows(UnavailableException.class, () ->
                 service.register(new registerUserRequest("Ann", "1234", "hi@gmail.com")));
@@ -49,7 +50,7 @@ public class UserServiceTest {
 
     //register user, positive test
     @Test
-    void registerUser() throws UnavailableException, DataAccessException {
+    void registerUser() throws UnavailableException, DataAccessException, InvalidLogInException {
         registerUserRequest userInfo = new registerUserRequest("Isaac", "password", "hi@gmail.com");
         service.register(userInfo);
         assert(service.getNumUsers() == 1);
@@ -58,7 +59,7 @@ public class UserServiceTest {
 
     //clear users, positive test
     @Test
-    void clearUsers() throws UnavailableException, DataAccessException {
+    void clearUsers() throws UnavailableException, DataAccessException, InvalidLogInException {
         registerUserRequest userInfo = new registerUserRequest("Isaac", "password", "hi@gmail.com");
         registerUserRequest userInfo2 = new registerUserRequest("Isaiah", "password", "hi@gmail.com");
         service.register(userInfo);
@@ -70,7 +71,7 @@ public class UserServiceTest {
 
     //log in and log out user positive test
     @Test
-    void logInAndOut() throws UnavailableException, DataAccessException, InvalidAuthTokenException {
+    void logInAndOut() throws UnavailableException, DataAccessException, InvalidAuthTokenException, InvalidLogInException {
         AuthService authServ = new AuthService(authDAO);
         registerUserRequest userInfo = new registerUserRequest("Bethany", "password", "hi@gmail.com");
         registerUserResult auth = service.register(userInfo);
