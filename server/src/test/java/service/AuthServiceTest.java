3c0e715c70e75329c77f221848f8734d58ef72de
RivetMaster <gish.loves.chocolate@gmail.com>
2026-06-08 16:43:51 -0400

    Change authservice tests to run for sql auth service. Remove the tests with duplicate usernames, same username multiple times is not meant to be supported.

--- server/src/test/java/service/AuthServiceTest.java
+++ server/src/test/java/service/AuthServiceTest.java
@@ -1,107 +1,140 @@
 package service;
 
-import dataaccess.AuthDAO;
-import dataaccess.AuthMemoryDAO;
+import dataaccess.*;
 
-import dataaccess.DataAccessException;
 import model.AuthData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import server.InvalidRequestException;
 import service.exceptions.InvalidAuthTokenException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AuthServiceTest {
-    static final AuthService SERVICE = new AuthService(new AuthMemoryDAO());
 
-    @BeforeEach
-    void clear() throws DataAccessException{
-        SERVICE.clearAuths();
+    private AuthDAO getDataAccess(Class<? extends AuthDAO> databaseClass) throws DataAccessException {
+        AuthDAO db;
+        if (databaseClass.equals(AuthSQLDAO.class)) {
+            db = new AuthSQLDAO();
+        } else {
+            db = new AuthMemoryDAO();
+        }
+        db.clearAuth(); //before each clear
+        return db;
     }
 
     //test correctly adds authtoken and verifies correctly
     //positive test for addAuth and verify auth
-    @Test
-    void addAuth() throws InvalidAuthTokenException, DataAccessException, InvalidRequestException {
-        AuthData auth1 = SERVICE.addAuth("Tim");
-        assert(SERVICE.verifyAuth(auth1.authToken()));
+    @ParameterizedTest
+    @ValueSource(classes = {AuthSQLDAO.class, AuthMemoryDAO.class})
+    void addAuth(Class<? extends AuthDAO> dao) throws InvalidAuthTokenException, DataAccessException, InvalidRequestException {
+        AuthDAO authDAO = getDataAccess(dao);
+        AuthService service = new AuthService(authDAO);
+
+        AuthData auth1 = service.addAuth("Tim");
+        assert(service.verifyAuth(auth1.authToken()));
     }
 
-    //test correctly adds and verifies multiple authorizations, username doesn't cause issues
+    //test correctly adds and verifies multiple authorizations
     //positive test for addAuth and verify Auth
-    @Test
-    void addAuths() throws InvalidAuthTokenException, DataAccessException, InvalidRequestException {
-        AuthData auth1 = SERVICE.addAuth("Tim");
-        AuthData auth2 = SERVICE.addAuth("Jen");
-        AuthData auth3 = SERVICE.addAuth("Tim");
-        AuthData auth4 = SERVICE.addAuth("Meg");
+    @ParameterizedTest
+    @ValueSource(classes = {AuthSQLDAO.class, AuthMemoryDAO.class})
+    void addAuths(Class<? extends AuthDAO> dao) throws InvalidAuthTokenException, DataAccessException, InvalidRequestException {
+        AuthDAO authDAO = getDataAccess(dao);
+        AuthService service = new AuthService(authDAO);
 
-        assert(SERVICE.verifyAuth(auth1.authToken()));
-        assert(SERVICE.verifyAuth(auth2.authToken()));
-        assert(SERVICE.verifyAuth(auth3.authToken()));
-        assert(SERVICE.verifyAuth(auth4.authToken()));
+        AuthData auth1 = service.addAuth("Tim");
+        AuthData auth2 = service.addAuth("Jen");
+        AuthData auth4 = service.addAuth("Meg");
+
+        assert(service.verifyAuth(auth1.authToken()));
+        assert(service.verifyAuth(auth2.authToken()));
+        assert(service.verifyAuth(auth4.authToken()));
     }
 
     //verifies authToken without being added causes invalid token exception
     //negative test for verify auth
-    @Test
-    void noAuth() {
+    @ParameterizedTest
+    @ValueSource(classes = {AuthSQLDAO.class, AuthMemoryDAO.class})
+    void noAuth(Class<? extends AuthDAO> dao) throws DataAccessException {
+        AuthDAO authDAO = getDataAccess(dao);
+        AuthService service = new AuthService(authDAO);
+
         String auth = AuthDAO.generateAuthToken();
 
-        assertThrows(InvalidAuthTokenException.class, () -> SERVICE.verifyAuth(auth));
+        assertThrows(InvalidAuthTokenException.class, () -> service.verifyAuth(auth));
     }
 
     //negative test for addAuth
-    @Test
-    void addAuthNull() {
-        assertThrows(InvalidRequestException.class, () -> SERVICE.addAuth(null));
+    @ParameterizedTest
+    @ValueSource(classes = {AuthSQLDAO.class, AuthMemoryDAO.class})
+    void addAuthNull(Class<? extends AuthDAO> dao) throws DataAccessException {
+        AuthDAO authDAO = getDataAccess(dao);
+        AuthService service = new AuthService(authDAO);
+
+        assertThrows(InvalidRequestException.class, () -> service.addAuth(null));
     }
 
     //verifies clearAuths works, authorizations don't stay past
     //positive test for clearAuths
-    @Test
-    void clearAuths() throws DataAccessException, InvalidRequestException {
-        AuthData auth1 = SERVICE.addAuth("Tim");
-        AuthData auth2 = SERVICE.addAuth("Jen");
-        AuthData auth3 = SERVICE.addAuth("Tim");
-        AuthData auth4 = SERVICE.addAuth("Meg");
+    @ParameterizedTest
+    @ValueSource(classes = {AuthSQLDAO.class, AuthMemoryDAO.class})
+    void clearAuths(Class<? extends AuthDAO> dao) throws DataAccessException, InvalidRequestException {
+        AuthDAO authDAO = getDataAccess(dao);
+        AuthService service = new AuthService(authDAO);
 
-        SERVICE.clearAuths();
+        AuthData auth1 = service.addAuth("Tim");
+        AuthData auth2 = service.addAuth("Jen");
+        AuthData auth4 = service.addAuth("Meg");
 
-        assertThrows(InvalidAuthTokenException.class, () -> SERVICE.verifyAuth(auth1.authToken()));
-        assertThrows(InvalidAuthTokenException.class, () -> SERVICE.verifyAuth(auth2.authToken()));
-        assertThrows(InvalidAuthTokenException.class, () -> SERVICE.verifyAuth(auth3.authToken()));
-        assertThrows(InvalidAuthTokenException.class, () -> SERVICE.verifyAuth(auth4.authToken()));
+        service.clearAuths();
+
+        assertThrows(InvalidAuthTokenException.class, () -> service.verifyAuth(auth1.authToken()));
+        assertThrows(InvalidAuthTokenException.class, () -> service.verifyAuth(auth2.authToken()));
+        assertThrows(InvalidAuthTokenException.class, () -> service.verifyAuth(auth4.authToken()));
     }
 
     //test delete authData throws error, leaves other authorizations alone
     //positive test for delAuth
-    @Test
-    void deleteAuth() throws DataAccessException, InvalidAuthTokenException, InvalidRequestException {
-        AuthData auth1 = SERVICE.addAuth("Ham");
-        AuthData auth2 = SERVICE.addAuth("Cheese");
+    @ParameterizedTest
+    @ValueSource(classes = {AuthSQLDAO.class, AuthMemoryDAO.class})
+    void deleteAuth(Class<? extends AuthDAO> dao) throws DataAccessException, InvalidAuthTokenException, InvalidRequestException {
+        AuthDAO authDAO = getDataAccess(dao);
+        AuthService service = new AuthService(authDAO);
 
-        SERVICE.delAuth(auth1.authToken());
+        AuthData auth1 = service.addAuth("Ham");
+        AuthData auth2 = service.addAuth("Cheese");
 
-        assertThrows(InvalidAuthTokenException.class, () -> SERVICE.verifyAuth(auth1.authToken()));
-        assert(SERVICE.verifyAuth(auth2.authToken()));
+        service.delAuth(auth1.authToken());
+
+        assertThrows(InvalidAuthTokenException.class, () -> service.verifyAuth(auth1.authToken()));
+        assert(service.verifyAuth(auth2.authToken()));
     }
 
     //test deleting non-existent authData throws error
     //negative test for delAuth
-    @Test
-    void deleteNonAuth() throws DataAccessException, InvalidAuthTokenException, InvalidRequestException {
-        AuthData auth1 = SERVICE.addAuth("Jim");
+    @ParameterizedTest
+    @ValueSource(classes = {AuthSQLDAO.class, AuthMemoryDAO.class})
+    void deleteNonAuth(Class<? extends AuthDAO> dao) throws DataAccessException, InvalidAuthTokenException, InvalidRequestException {
+        AuthDAO authDAO = getDataAccess(dao);
+        AuthService service = new AuthService(authDAO);
+
+        AuthData auth1 = service.addAuth("Jim");
         AuthData auth2 = new AuthData(AuthDAO.generateAuthToken(), "Mary");
 
-        assertThrows(InvalidAuthTokenException.class,() -> SERVICE.delAuth(auth2.authToken()));
-        assert(SERVICE.verifyAuth(auth1.authToken()));
+        assertThrows(InvalidAuthTokenException.class,() -> service.delAuth(auth2.authToken()));
+        assert(service.verifyAuth(auth1.authToken()));
     }
 
-    @Test
-    void nullAuth(){
-        assertThrows(InvalidAuthTokenException.class, () -> SERVICE.verifyAuth(null));
+    @ParameterizedTest
+    @ValueSource(classes = {AuthSQLDAO.class, AuthMemoryDAO.class})
+    void nullAuth(Class<? extends AuthDAO> dao) throws DataAccessException {
+        AuthDAO authDAO = getDataAccess(dao);
+        AuthService service = new AuthService(authDAO);
+
+        assertThrows(InvalidAuthTokenException.class, () -> service.verifyAuth(null));
     }
 
 
