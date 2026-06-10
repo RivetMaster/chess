75bfe5798e95722d6752da94d7728e140940df90
RivetMaster <gish.loves.chocolate@gmail.com>
2026-06-10 18:24:18 -0400

    Create ResponseException class (copied from PetShop). Create ServerFacade class and implement clear, register, and logIn functions. Test them manually with some code in ServerFacadeTests, basic function implemented.

--- client/src/main/test/java/client/ServerFacadeTests.java
+++ client/src/main/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,26 @@
 package client;
 
+import exceptions.ResponseException;
 import org.junit.jupiter.api.*;
+import resultsandrequests.LogInRequest;
+import resultsandrequests.RegisterUserRequest;
 import server.Server;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        var url = "http://localhost:" + port;
+        serverFacade = new ServerFacade(url);
     }
 
     @AfterAll
@@ -21,9 +29,26 @@ public class ServerFacadeTests {
     }
 
 
-    @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    @BeforeEach
+    public void clear() throws ResponseException {
+        //manually add values to db
+        serverFacade.clear();
     }
+
+    @Test
+    public void registerUser() throws ResponseException {
+        serverFacade.register(new RegisterUserRequest("Hippo", "GrassEater", "ManKiller!"));
+        assertThrows(ResponseException.class, () ->
+                serverFacade.register(new RegisterUserRequest("Hippo", "Grass2Eater", "Man3Killer!")));
+
+    }
+
+    @Test
+    public void logIn() throws ResponseException {
+        serverFacade.register(new RegisterUserRequest("Jake", "Wakey", "bakeSale@"));
+        serverFacade.logIn(new LogInRequest("Jake", "Wakey"));
+    }
+
+
 
 }
