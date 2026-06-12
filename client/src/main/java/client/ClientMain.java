ded75e996fa510032d6e254c879da1d3ba1e93eb
RivetMaster <gish.loves.chocolate@gmail.com>
2026-06-12 11:46:50 -0400

    Implement text scanning and responses for signed in and signed out. Can draw the board and can leave the game, though it doesn't remove the player from the game.

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -2,13 +2,14 @@ package client;
 
 import chess.*;
 import resultsandrequests.UIResponse;
+import static ui.EscapeSequences.*;
+
 
 import java.util.Scanner;
 
 import static client.ClientMain.State.*;
 
 public class ClientMain {
-    private static ServerFacade server;
     private State state;
     private static ClientUI ui;
     private String authToken;
@@ -20,9 +21,11 @@ public class ClientMain {
     }
 
     public static void main(String[] args) {
-        System.out.println("♕ Welcome to the 240 Chess Client. Type Help for help getting started. ♕" );
+        System.out.println("♕ Welcome to the 240 Chess Client. Type "+ SET_TEXT_BOLD+ "HELP"
+            + RESET_TEXT_BOLD_FAINT + " for help getting started. ♕" );
+
         var url = "http://localhost:" + 8080;
-        server = new ServerFacade(url);
+        ServerFacade server = new ServerFacade(url);
         ui = new ClientUI(server);
 
         ClientMain client = new ClientMain();
@@ -30,45 +33,68 @@ public class ClientMain {
     }
 
     private void scanInput(){
+        boolean TESTING = true;
         Scanner scanner = new Scanner(System.in);
         state = SIGNED_OUT;
+
         //loop with scanner object
         while(true){
-            System.out.print(">>>>");
-            String line = scanner.nextLine();
-            //should be able to quit from anywhere in the program? Have so if logged in and quit logs out first?
-            if(state == SIGNED_OUT &&
+            System.out.print(">>>> ");
+            String line = scanner.nextLine().trim();
+            StringBuilder output = new StringBuilder();
+
+            //split line into words
+            String[] words = line.split(" ");
+
+            //if nothing was entered
+            if(words.length == 0){
+                output.append("Type ").append(ClientUI.bold(  "HELP")).append(" to see a list of valid commands.");
+            }
+            //QUIT when signed out
+            else if(state == SIGNED_OUT &&
                     (line.equalsIgnoreCase("quit") || line.equalsIgnoreCase("q"))){
                 break;
             }
-            if(line.equalsIgnoreCase("help") || line.equalsIgnoreCase("h")){
-                System.out.println(ui.helpMenu(state));
+            //help
+            else if(line.equalsIgnoreCase("help") || line.equalsIgnoreCase("h")){
+                output.append(ui.helpMenu(state));
             }
+            //clear (TESTING only)
+            else if(TESTING && line.equalsIgnoreCase("clear")){
+                output.append(ui.clear());
+                state = SIGNED_OUT;
+            }
+            //Signed out options
             else if(state == SIGNED_OUT){
-                System.out.println(signedOutREPL(line));
+                output.append(signedOutREPL(words));
             }
-        }
-        //can always access help, so check if is help first
-        //if SIGNED_OUT send to signed out repl
-        //if signed_in send to signed in repl
-        //if in-game send to game repl
+            //signed in options
+            else if(state == SIGNED_IN){
+                output.append(signedInREPL(words));
+            }
+            //in game options
+            else if(state == IN_GAME){
+                output.append(inGameREPL(words));
+            }
 
+            //if output comes back empty
+            if(output.isEmpty()){
+                //invalid command
+                output.append("Type ").append(ClientUI.bold(  "HELP")).append(" to see a list of valid commands.");
+            }
+            System.out.println(output);
+        }
     }
 
-    private String signedOutREPL(String line){
-        String[] words = line.split(" ");
+    private String signedOutREPL(String[] words){
+
         StringBuilder reply = new StringBuilder();
-        String command = "";
+        String command  = words[0];
 
-        if(words.length == 0){
-            reply.append("Type HELP to see a list of valid commands.");
-        } else{
-            command = words[0];
-        }
-
+        //REGISTER
         if(command.equalsIgnoreCase("register")){
             if(words.length != 4){
-                reply.append("Expecting register <USERNAME> <PASSWORD> <EMAIL>");
+                reply.append("Expecting ").append(ClientUI.bold("register <USERNAME> <PASSWORD> <EMAIL>"));
             } else{
                 UIResponse response = ui.register(words[1], words[2], words[3]);
                 reply.append(response.message());
@@ -77,11 +103,106 @@ public class ClientMain {
                     state = SIGNED_IN;
                 }
             }
-        } else if(command.equalsIgnoreCase("login")){
-            reply.append("Trying to login...");
-        } else if(reply.isEmpty()){
-            reply.append("Type HELP to see a list of valid commands.");
         }
+        //LOGIN
+        else if(command.equalsIgnoreCase("login")){
+            if(words.length != 3){
+                reply.append("Expecting ").append(ClientUI.bold("login <USERNAME> <PASSWORD>"));
+            } else{
+                UIResponse response = ui.login(words[1], words[2]);
+                reply.append(response.message());
+                if(response.authToken() != null) {
+                    authToken = response.authToken();
+                    state = SIGNED_IN;
+                }
+            }
+        }
+        return reply.toString();
+    }
+
+    private String signedInREPL(String[] words){
+        StringBuilder reply = new StringBuilder();
+        String command = words[0];
+
+        if(command.equalsIgnoreCase("logout")){
+            if(words.length != 1){
+                reply.append("Expecting ").append(ClientUI.bold("logout"));
+            } else{
+                UIResponse response = ui.logout(authToken);
+                reply.append(response.message());
+                if(response.authToken() == null) {
+                    authToken = null;
+                    state = SIGNED_OUT;
+                }
+            }
+        } else if(command.equalsIgnoreCase("create")){
+            if(words.length != 2){
+                reply.append("Expecting ").append(ClientUI.bold("create <NAME>"));
+            } else{
+                String response = ui.create(words[1], authToken);
+                reply.append(response);
+            }
+        } else if(command.equalsIgnoreCase("list")){
+            if(words.length != 1){
+                reply.append("Expecting ").append(ClientUI.bold("list"));
+            } else{
+                String response = ui.list(authToken);
+                reply.append(response);
+            }
+        } else if(command.equalsIgnoreCase("join")){
+            if(words.length != 3){
+                reply.append("Expecting ").append(ClientUI.bold( "join <ID> <COLOR>"));
+            } else{
+                try{
+                    int id = Integer.parseInt(words[1]);
+                    ChessGame.TeamColor color = ChessGame.TeamColor.valueOf(words[2].toUpperCase());
+                    UIResponse response = ui.join(id, color, authToken);
+                    reply.append(response.message());
+                    if(response.authToken() != null) {
+                        authToken = response.authToken();
+                        state = IN_GAME;
+                    }
+                } catch (NumberFormatException e) {
+                    reply.append(ClientUI.red("Error: Invalid Game ID"));
+                } catch(IllegalArgumentException e){
+                    reply.append(ClientUI.red("Error: Invalid color. Expecting " + ClientUI.bold("WHITE")
+                            + " or " + ClientUI.bold("BLACK") + "."));
+                }
+            }
+        } else if(command.equalsIgnoreCase("observe")){
+            if(words.length != 2){
+                reply.append("Expecting ").append(ClientUI.bold("observe <ID>"));
+            } else{
+                try{
+                    int id = Integer.parseInt(words[1]);
+                    UIResponse response = ui.observe(id, authToken);
+                    reply.append(response.message());
+                    if(response.authToken() != null) {
+                        authToken = response.authToken();
+                        state = IN_GAME;
+                    }
+                } catch (NumberFormatException e) {
+                    reply.append(ClientUI.red("Error: Invalid Game ID"));
+                }
+            }
+        }
+        return reply.toString();
+    }
+
+    private String inGameREPL(String[] words){
+        StringBuilder reply = new StringBuilder();
+        String command = words[0];
+
+        if(command.equalsIgnoreCase("leave")){
+            if(words.length != 1){
+                reply.append("Expecting ").append(ClientUI.bold("leave"));
+            }
+            else{
+                reply.append("Left game.");
+                state = SIGNED_IN;
+            }
+        }
+
         return reply.toString();
     }
 }
--- client/src/main/java/client/ClientUI.java
+++ client/src/main/java/client/ClientUI.java
@@ -1,37 +1,61 @@
 package client;
 
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
 import exceptions.ResponseException;
-import resultsandrequests.RegisterUserRequest;
-import resultsandrequests.RegisterUserResult;
-import resultsandrequests.UIResponse;
+import model.AuthData;
+import model.GameData;
+import resultsandrequests.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static chess.ChessGame.TeamColor.BLACK;
+import static chess.ChessGame.TeamColor.WHITE;
 import static client.ClientMain.State.*;
+import static ui.EscapeSequences.*;
 
 public class ClientUI {
     ServerFacade serverFacade;
+    Map<Integer, Integer> gameIDs;
 
     public ClientUI(ServerFacade serverFacade){
         this.serverFacade = serverFacade;
+        gameIDs = new HashMap<>();
     }
 
 
     public String helpMenu(ClientMain.State state){
         StringBuilder uiOutput = new StringBuilder();
         if(state == SIGNED_OUT){
-            uiOutput.append("REGISTER <Username> <Password> <Email> : To create an account\n");
-            uiOutput.append("LOGIN <Username> <Password> : To log in to an existing account\n");
-            uiOutput.append("QUIT : Exit Program\n");
+            uiOutput.append(bold("REGISTER <Username> <Password> <Email> ")).append(": To create an account\n");
+            uiOutput.append(bold("LOGIN <Username> <Password>            ")).append(": To log in to an existing account\n");
+            uiOutput.append(bold("QUIT                                   ")).append(": Exit Program\n");
+            uiOutput.append(bold("HELP                                   ")).append(": See List of Commands\n");
         } else if(state == SIGNED_IN){
-            uiOutput.append("CREATE <name> : Create a new chess game with a name\n");
-            uiOutput.append("LIST : See a list of all chess games\n");
-            uiOutput.append("JOIN <ID> <COLOR> : Join a game as BLACK or WHITE\n");
-            uiOutput.append("OBSERVE <ID> : Choose a game to watch\n");
-            uiOutput.append("LOGOUT : Sign out of account\n");
+            uiOutput.append(bold("CREATE <name>     ")).append(": Create a new chess game with a name\n");
+            uiOutput.append(bold("LIST              ")).append(": See a list of all chess games\n");
+            uiOutput.append(bold("JOIN <ID> <COLOR> ")).append(": Join a game as BLACK or WHITE\n");
+            uiOutput.append(bold("OBSERVE <ID>      ")).append(": Choose a game to watch\n");
+            uiOutput.append(bold("LOGOUT            ")).append(": Sign out of account\n");
+            uiOutput.append(bold("HELP              ")).append(": See List of Commands\n");
         } else if(state == IN_GAME){
             //TO DO
+            uiOutput.append(bold("LEAVE ")).append(": Leave game\n");
+            uiOutput.append(bold("HELP  ")).append(": See List of Commands\n");
         }
-        uiOutput.append("HELP : See List of Commands\n");
         return uiOutput.toString();
+    }
+
+    public static String bold(String text){
+        return SET_TEXT_BOLD + text + RESET_TEXT_BOLD_FAINT;
+    }
+
+    public static String red(String text){
+        return SET_TEXT_COLOR_RED + text + RESET_TEXT_COLOR;
     }
 
     public UIResponse register(String username, String password, String email){
@@ -41,9 +65,219 @@ public class ClientUI {
             return new UIResponse("Account successfully made and signed in as " + result.username(), result.authToken());
         } catch (ResponseException e){
             if(e.code() == ResponseException.Code.ServerError) {
-                return new UIResponse("Error: Could not connect to the server.", null);
+                return new UIResponse(red("Error: Could not connect to the server."), null);
             }
-            return new UIResponse(e.getMessage(), null);
+            return new UIResponse(red(e.getMessage()), null);
         }
+    }
+
+    public UIResponse login(String username, String password){
+        LogInRequest request = new LogInRequest(username, password);
+        try{
+            LogInResult result = serverFacade.logIn(request);
+            return new UIResponse("Successfully signed in as " + result.username(), result.authToken());
+        } catch (ResponseException e){
+            if(e.code() == ResponseException.Code.ServerError) {
+                return new UIResponse(red("Error: Could not connect to the server."), null);
+            }
+            return new UIResponse(red(e.getMessage()), null);
+        }
+    }
+
+    public String clear(){
+        try{
+            serverFacade.clear();
+            return "Successfully cleared database.";
+        } catch (ResponseException e){
+            if(e.code() == ResponseException.Code.ServerError) {
+                return red("Error: Could not connect to the server.");
+            }
+            return red(e.getMessage());
+        }
+    }
+
+    public UIResponse logout(String authToken){
+        try{
+            serverFacade.logOut(new LogOutRequest(authToken));
+            return new UIResponse("Successfully logged out.", null);
+        } catch( ResponseException e){
+            if(e.code() == ResponseException.Code.ServerError) {
+                return new UIResponse(red("Error: Could not connect to the server."), authToken);
+            }
+            return new UIResponse(red(e.getMessage()), authToken);
+        }
+    }
+
+    public String create(String gameName, String authToken){
+        try{
+           CreateGameResult result = serverFacade.createGame(new CreateGameRequest(gameName, authToken));
+           updateGameIDList(authToken);
+           return "Successfully created a new chess game with ID " + getKeyFromID(result.gameID());
+        } catch(ResponseException e){
+            if(e.code() == ResponseException.Code.ServerError) {
+                return red("Error: Could not connect to the server.");
+            }
+            return red(e.getMessage());
+        }
+    }
+
+    public String list(String authToken){
+        try{
+            ListGamesResult result = serverFacade.listGames(new ListGamesRequest(authToken));
+            updateGameIDList(authToken);
+
+            StringBuilder output = new StringBuilder();
+            output.append(bold("Game ID")).append(" | ").append(bold("Game Name")).append(" | <").append(bold("White Team"))
+                    .append(", ").append(bold("Black Team")).append(">\n");
+            for(int i = 0; i < gameIDs.size(); i++){ //for every key in gameIDs (1 to i)
+                output.append(i+1).append("       | ");
+                for(int k = 0; k < result.games().size(); k++){ //check every game returned by listGames
+                    if(result.games().get(k).gameID() == gameIDs.get(i+1)){ //if the id of the game in list games corresponds to the key's value
+                        output.append(result.games().get(k).gameName()).append("    | <");
+                        if(result.games().get(k).whiteUsername() == null){
+                            output.append("AVAILABLE, ");
+                        } else{
+                            output.append(result.games().get(k).whiteUsername()).append(", ");
+                        }
+                        if(result.games().get(k).blackUsername() == null){
+                            output.append("AVAILABLE");
+                        } else{
+                            output.append(result.games().get(k).blackUsername());
+                        }
+                        output.append(">\n");
+                        break;
+                    }
+                }
+            }
+            return output.toString();
+        } catch(ResponseException e){
+            if(e.code() == ResponseException.Code.ServerError) {
+                return red("Error: Could not connect to the server.");
+            }
+            return red(e.getMessage());
+        }
+    }
+
+    public UIResponse join(int id, ChessGame.TeamColor color, String authToken) {
+        if(!gameIDs.containsKey(id)){
+            return new UIResponse(ClientUI.red("Error: Invalid Game ID"), null);
+        }
+        try{
+            serverFacade.joinGame(new JoinGameRequest(gameIDs.get(id), color, new AuthData(authToken, null)));
+            return new UIResponse("Successfully joined game " + id +" as team "
+                    +color + ".\n" + printBoard(gameIDs.get(id), color, authToken), authToken);
+        } catch(ResponseException e){
+            if(e.code() == ResponseException.Code.ServerError) {
+                return new UIResponse(red("Error: Could not connect to the server."), null);
+            }
+            return new UIResponse(red(e.getMessage()), null);
+        }
+    }
+
+    public UIResponse observe(int id, String authToken){
+        if(!gameIDs.containsKey(id)){
+            return new UIResponse(ClientUI.red("Error: Invalid Game ID"), null);
+        }
+        try {
+            return new UIResponse("Now observing game " + id + "\n" + printBoard(gameIDs.get(id), WHITE, authToken), authToken);
+        } catch(ResponseException e){
+            if(e.code() == ResponseException.Code.ServerError) {
+                return new UIResponse(red("Error: Could not connect to the server."), null);
+            }
+            return new UIResponse(red(e.getMessage()), null);
+        }
+    }
+
+    private void updateGameIDList(String authToken) throws ResponseException{
+        ListGamesResult result = serverFacade.listGames(new ListGamesRequest(authToken));
+        gameIDs.clear();
+        for(int i = 0; i < result.games().size(); i++){
+            gameIDs.put(i+1, result.games().get(i).gameID());
+        }
+    }
+
+    private int getKeyFromID(int gameID) throws ResponseException{
+        for(Map.Entry<Integer, Integer> game : gameIDs.entrySet()) {
+            // Find the value that matches gameID, return the key
+            if(game.getValue() == gameID){
+               return game.getKey();
+            }
+        }
+        throw new ResponseException(ResponseException.Code.ServerError, "Error: Could not create game.");
+    }
+
+    private String printBoard(int gameID, ChessGame.TeamColor color, String authToken) throws ResponseException{
+        StringBuilder board = new StringBuilder();
+        ChessGame chessGame = getBoard(gameID, authToken);
+        ChessBoard chessBoard = chessGame.getBoard();
+        //Boarder Above
+        if(color == WHITE){
+            board.append(boarderColor("    a  b  c  d  e  f  g  h    ")).append("\n");
+            board.append(drawBoard(chessBoard, WHITE));
+            board.append(boarderColor("    a  b  c  d  e  f  g  h    ")).append("\n");
+        } else if(color == BLACK){
+            board.append(boarderColor("    h  g  f  e  d  c  b  a    ")).append("\n");
+            board.append(drawBoard(chessBoard, BLACK));
+            board.append(boarderColor("    h  g  f  e  d  c  b  a    ")).append("\n");
+
+        }
+
+
+        return board.toString();
+    }
+
+    private String boarderColor(String text){
+        return SET_BG_COLOR_LIGHT_GREY +SET_TEXT_COLOR_BLUE + text + RESET_BG_COLOR +RESET_TEXT_COLOR;
+    }
+
+    private ChessGame getBoard(int gameID, String authToken) throws ResponseException {
+        ArrayList<GameData> games = serverFacade.listGames(new ListGamesRequest(authToken)).games();
+        for (GameData game : games) {
+            if (game.gameID() == gameID) {
+                return game.game();
+            }
+        }
+        throw new ResponseException(ResponseException.Code.ClientError, "Error: Game Does Not Exist");
+    }
+
+    private String drawBoard(ChessBoard board, ChessGame.TeamColor color){
+        StringBuilder boardRow = new StringBuilder();
+        if(color == WHITE){
+            for(int row = 8; row > 0; row--) {
+                boardRow.append(boarderColor(" " + row +" "));
+                for (int col = 1; col <= 8; col++) {
+                    if (col % 2 == row % 2) {
+                        boardRow.append(SET_BG_COLOR_WHITE); //black square
+                    }
+                    ChessPiece piece = board.getPiece(new ChessPosition(row, col));
+                    if(piece != null) {
+                        boardRow.append(board.getPiece(new ChessPosition(row, col)).toStringBoard());
+                    } else{
+                        boardRow.append(EMPTY);
+                    }
+                    boardRow.append(RESET_BG_COLOR);
+                }
+                boardRow.append(boarderColor(" " + row +" ")).append("\n");
+            }
+        } else if(color == BLACK){
+            for(int row = 1; row <= 8; row++) {
+                boardRow.append(boarderColor(" " + row +" "));
+                for (int col = 8; col > 0 ; col--) {
+                    if (col % 2 == row % 2) {
+                        boardRow.append(SET_BG_COLOR_WHITE); //black square
+                    }
+                    ChessPiece piece = board.getPiece(new ChessPosition(row, col));
+                    if(piece != null) {
+                        boardRow.append(board.getPiece(new ChessPosition(row, col)).toStringBoard());
+                    } else{
+                        boardRow.append(EMPTY);
+                    }
+                    boardRow.append(RESET_BG_COLOR);
+                }
+                boardRow.append(boarderColor(" " + row +" ")).append("\n");
+            }
+        }
+
+        return boardRow.toString();
     }
 }
